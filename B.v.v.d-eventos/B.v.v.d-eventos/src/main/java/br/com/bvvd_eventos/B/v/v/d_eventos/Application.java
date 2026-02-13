ea0f6f6b18bd41afbc81452d0b993dac812765ee
telesvitor <vitor.teles.pereira@escola.pr.gov.br>
2026-02-13 11:25:39 -0300

    Add files via upload

---
+++ B.v.v.d-eventos/B.v.v.d-eventos/src/main/java/br/com/bvvd_eventos/B/v/v/d_eventos/Application.java
@@ -0,0 +1,13 @@
+package br.com.bvvd_eventos.B.v.v.d_eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ B.v.v.d-eventos/B.v.v.d-eventos/src/test/java/br/com/bvvd_eventos/B/v/v/d_eventos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bvvd_eventos.B.v.v.d_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
